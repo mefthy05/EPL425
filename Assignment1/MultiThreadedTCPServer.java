65db4543d725ea5e37507bcfac38c3ee3716f1c9
iandre14 <iandre14@cs.ucy.ac.cy>
2017-10-06 19:05:20 +0300

    Update MultiThreadedTCPServer.java

--- Assignment1/MultiThreadedTCPServer.java
+++ Assignment1/MultiThreadedTCPServer.java
@@ -7,48 +7,44 @@ import java.net.Socket;
 import java.util.Date;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-
-public class MultiThreadedTCPServer {
-    private static class TCPWorker implements Runnable {
-		private Socket client;
+public class MultiThreadedTCPServer { 
+	private static class TCPWorker implements Runnable {
+		private Socket client;  
 		private String clientbuffer;
-		public TCPWorker(Socket client) {
-			this.client = client;
-			this.clientbuffer = "";
-		}
-
-		@Override
-		public void run() {
-			try {
-				System.out.println("Client connected with: " + this.client.getInetAddress());
-				DataOutputStream output = new DataOutputStream(client.getOutputStream());
-				BufferedReader reader = new BufferedReader(new InputStreamReader(this.client.getInputStream()));
-				while (client.isConnected() && !client.isClosed()){
-					this.clientbuffer = reader.readLine();
-					if (this.clientbuffer.equals("FIN" + System.lineSeparator())){break;}
-					System.out.println("[" + new Date() + "] Received: " + this.clientbuffer);
-					output.writeBytes(this.clientbuffer.toUpperCase() + System.lineSeparator());
-				}
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
-	}
-    
-    public static ExecutorService TCP_WORKER_SERVICE = Executors.newFixedThreadPool(10);
-
-	public static void main(String args[]) {
-		try {
-			@SuppressWarnings("resource")
-			ServerSocket socket = new ServerSocket(80);
-			System.out.println("Server listening to: " + socket.getInetAddress() + ":" + socket.getLocalPort());
-			while (true) {
-				Socket client = socket.accept();
-				TCP_WORKER_SERVICE.submit(new TCPWorker(client));
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-}
-
+  public TCPWorker(Socket client) {
+	  this.client = client; 
+	  this.clientbuffer = "";  
+  }
+  @Override  
+		public void run() { 
+			try {  
+				System.out.println("Client connected with: " + this.client.getInetAddress());    
+				DataOutputStream output = new DataOutputStream(client.getOutputStream()); 
+				BufferedReader reader = new BufferedReader(new InputStreamReader(this.client.getInputStream())); 
+				while (client.isConnected() && !client.isClosed()) {     
+					this.clientbuffer = reader.readLine(); 
+					if (this.clientbuffer.equals("FIN" + System.lineSeparator())) { 
+						break;     }     System.out.println("[" + new Date() + "] Received: " + this.clientbuffer);
+					
+     output.writeBytes(this.clientbuffer.toUpperCase() + System.lineSeparator()); 
+				}   
+			} catch (IOException e) {    e.printStackTrace(); 
+						}  } }
+ public static ExecutorService TCP_WORKER_SERVICE = Executors.newFixedThreadPool(10);
+	
+ public static void main(String args[]) {
+	 try {   
+		 @SuppressWarnings("resource")   ServerSocket socket = new ServerSocket(80); 
+		 System.out.println("Server listening to: " + socket.getInetAddress() + ":" + socket.getLocalPort());
+   int requests = 0;
+   long startTime = System.currentTimeMillis();
+   while (true) {  
+	   Socket client = socket.accept();    
+	   TCP_WORKER_SERVICE.submit(new TCPWorker(client)); 
+	   requests++;   }
+   long endTime = System.currentTimeMillis();
+   long totalTime = endTime - startTime; 
+		 long throughput = requests / totalTime;
+   System.out.println("\nThroughput is: " + throughput);
+  } catch (IOException e) {   e.printStackTrace(); 
+			  } }}
