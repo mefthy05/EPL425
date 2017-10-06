abf2603c6e7be360447cdccf4d7960c0b5d14952
iandre14 <iandre14@cs.ucy.ac.cy>
2017-10-06 18:37:44 +0300

    Update TCPClient.java

--- Assignment1/TCPClient.java
+++ Assignment1/TCPClient.java
@@ -6,34 +6,47 @@ import java.net.Socket;
 import java.util.Date;
 import java.util.Random;
 import java.util.Scanner;
-
-public class TCPClient {
-   static final int MIN_REQUESTS = 300;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+　
+public class TCPclient {
+	static final int MIN_REQUESTS = 300;
+	static String ip = "";
+	private static class User implements Runnable{
+		@Override
+		public void run() {
+			try {
+	            String message, response;
+	            Socket socket = new Socket(ip, 80);
+	            DataOutputStream output = new DataOutputStream(socket.getOutputStream());
+	            BufferedReader server = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+	            message = "HELLO " + socket.getLocalAddress() + " " + socket.getLocalPort() + " " /*+ n*/ + System.lineSeparator();
+	            System.out.println(message);
+	            output.writeBytes(message);
+	            response = server.readLine();
+	            Random ran = new Random();
+	            for(int i = 0; i < ran.nextInt(100) + MIN_REQUESTS; i++){
+	            	output.writeBytes(message);
+	            	response = server.readLine();
+	                System.out.println("[" + new Date() + "] Received: " + response);
+	            }
+	            output.writeBytes("FIN" + System.lineSeparator());
+	            socket.close();
+	        } catch (IOException e) {
+	            e.printStackTrace();
+	        }
+		}
+	}
+	
+	//public static ExecutorService USER_POOL = Executors.newFixedThreadPool(10);
+	
     public static void main(String args[]) {
     	Scanner inpt = new Scanner(System.in);
     	System.out.print("Give server IP: ");
-    	String ip = inpt.nextLine();
+    	ip = inpt.nextLine();
     	int n = 0;
-        try {
-            String message, response;
-            Socket socket = new Socket(ip, 80);
-            DataOutputStream output = new DataOutputStream(socket.getOutputStream());
-            BufferedReader server = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            message = "HELLO " + socket.getLocalAddress() + " " + socket.getLocalPort() + " " + n + System.lineSeparator();
-            System.out.println(message);
-            output.writeBytes(message);
-            response = server.readLine();
-            Random ran = new Random();
-            for(int i = 0; i < ran.nextInt(100) + MIN_REQUESTS; i++){
-            	output.writeBytes(message);
-            	response = server.readLine();
-                System.out.println("[" + new Date() + "] Received: " + response);
-            }
-            output.writeBytes("FIN" + System.lineSeparator());
-            socket.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        
         inpt.close();
     }
 }
+　
