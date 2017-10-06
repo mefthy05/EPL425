ee5f3542ccb1e12f9f4ee411eb1a21fc6dda1f51
mefthy05 <mefthy05@ucy.ac.cy>
2017-10-06 18:24:56 +0300

    Update TCPClient.java

--- Assignment1/TCPClient.java
+++ Assignment1/TCPClient.java
@@ -4,23 +4,32 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.Socket;
 import java.util.Date;
+import java.util.Random;
 import java.util.Scanner;
 
 public class TCPClient {
+   static final int MIN_REQUESTS = 300;
     public static void main(String args[]) {
-        Scanner inpt = new Scanner(System.in);
+    	Scanner inpt = new Scanner(System.in);
     	System.out.print("Give server IP: ");
     	String ip = inpt.nextLine();
+    	int n = 0;
         try {
             String message, response;
             Socket socket = new Socket(ip, 80);
             DataOutputStream output = new DataOutputStream(socket.getOutputStream());
             BufferedReader server = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-            message = reader.readLine() + System.lineSeparator();
+            message = "HELLO " + socket.getLocalAddress() + " " + socket.getLocalPort() + " " + n + System.lineSeparator();
+            System.out.println(message);
             output.writeBytes(message);
             response = server.readLine();
-            System.out.println("[" + new Date() + "] Received: " + response);
+            Random ran = new Random();
+            for(int i = 0; i < ran.nextInt(100) + MIN_REQUESTS; i++){
+            	output.writeBytes(message);
+            	response = server.readLine();
+                System.out.println("[" + new Date() + "] Received: " + response);
+            }
+            output.writeBytes("FIN" + System.lineSeparator());
             socket.close();
         } catch (IOException e) {
             e.printStackTrace();
