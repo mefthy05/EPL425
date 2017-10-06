5a85bb631bcc55eb326fb8d640eb8353329abd17
mefthy05 <mefthy05@ucy.ac.cy>
2017-10-06 16:38:09 +0300

    Update TCPClient.java

--- Assignment1/TCPClient.java
+++ Assignment1/TCPClient.java
@@ -7,37 +7,24 @@ import java.util.Date;
 import java.util.Scanner;
 
 public class TCPClient {
-
     public static void main(String args[]) {
         Scanner inpt = new Scanner(System.in);
     	System.out.print("Give server IP: ");
     	String ip = inpt.nextLine();
         try {
-
             String message, response;
             Socket socket = new Socket(ip, 80);
-
             DataOutputStream output = new DataOutputStream(socket.getOutputStream());
-            BufferedReader server = new BufferedReader(
-                    new InputStreamReader(socket.getInputStream())
-            );
-
-            BufferedReader reader = new BufferedReader(
-                    new InputStreamReader(System.in)
-            );
+            BufferedReader server = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
             message = reader.readLine() + System.lineSeparator();
-
-
             output.writeBytes(message);
             response = server.readLine();
-
             System.out.println("[" + new Date() + "] Received: " + response);
             socket.close();
-
         } catch (IOException e) {
             e.printStackTrace();
         }
         inpt.close();
     }
-
 }
