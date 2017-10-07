e513f9b9abe2f378173203757d08ad0211422307
mefthy05 <mefthy05@ucy.ac.cy>
2017-10-07 17:37:10 +0300

    Update MultiThreadedTCPServer.java

--- Assignment1/MultiThreadedTCPServer.java
+++ Assignment1/MultiThreadedTCPServer.java
@@ -8,7 +8,7 @@ import java.util.Date;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-public class Server {
+public class MultiThreadedTCPServer {
 	private static class TCPWorker implements Runnable {
 		private Socket client;
 		private String clientbuffer;
